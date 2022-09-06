b8c5c3b08756e8296060c5cce3e094870500a7c2
Amardeep Singh <81309641+AmardeepSingh1301@users.noreply.github.com>
2022-09-06 17:43:51 +0530

    Add files via upload

---
+++ SWG.py
@@ -0,0 +1,44 @@
+# from playsound import playsound
+# playsound('C:\\Users\\Amar Singh\\Downloads\\close.mp3')
+import random
+def game(comp,you):
+    if comp==you:
+        return None
+    elif comp=='s':
+        if you=='p':
+            return True
+        elif you=='c':
+            return False
+    
+    elif comp=='p':
+        if you=='c':
+            return True
+        elif you=='s':
+            return False
+
+    elif comp=='c':
+        if you=='s':
+            return True
+        elif you=='p':
+            return False
+
+print("Welcome to stone, paper, secissor game\n")
+print("Computer chooses stone(s), Paper(p), secissor(c)")
+rand=random.randint(1,3)
+if rand==1:
+    comp='s'
+elif rand==2:
+    comp='p'
+else:
+    comp='c'
+
+you=input("Enter your choice Stone(s), Paper(p), secissor(c)\n")
+a=game(comp,you)
+print("Computer choose "+ comp)
+print("You Choose "+ you)
+if a==None:
+    print("Game is tie")
+elif a:
+    print("You Won :)")
+else:
+    print("You Lose :(")
